ad9c3a8648286114974467d38f0b97c20c609d7f
sixolisiwe <sxlswmcb@gmail.com>
2019-05-17 14:39:04 +0200

    closed regcheck 3rd function

--- Test/regCheck.test.js
+++ Test/regCheck.test.js
@@ -1,17 +1,17 @@
-describe('regCheck' , function(){
-it('should return true if a registration number ends with GP', function(){  
-assert.equal(regCheck('DV 23 NB GP', 'GP'), true);
-    });
-it('should return false if a registration number ends with MP', function(){  
-assert.equal(regCheck('DV 23 LP GP', 'MP'), false);
-   });
-it('should return true if a registration number starts with CY', function(){  
-assert.equal(regCheck('CY189-012', 'CY'), true);
+describe('regCheck', function () {
+  it('should return true if a registration number ends with GP', function () {
+    assert.equal(regCheck('DV 23 NB GP', 'GP'), true);
   });
-it('should return false if a registration number ends with ND', function(){  
-assert.equal(regCheck('CY189-012', 'ND'), false)
-it('should return false if a registration number is an empty string', function(){
-assert.equal(regCheck("",""), false);
-});
-});
+  it('should return false if a registration number ends with MP', function () {
+    assert.equal(regCheck('DV 23 LP GP', 'MP'), false);
+  });
+  it('should return true if a registration number starts with CY', function () {
+    assert.equal(regCheck('CY189-012', 'CY'), true);
+  });
+  it('should return false if a registration number ends with ND', function () {
+    assert.equal(regCheck('CY189-012', 'ND'), false)
+  });
+  it('should return false if a registration number is an empty string', function () {
+    assert.equal(regCheck("", ""), false);
+  });
 });
